e41c439e499a99e3be04f3d2127c056e8f7e48f9
vinux <vinux.rich@gmail.com>
2017-10-05 15:21:40 +0530

    updated site

---
+++ vendor/bundle/gems/jekyll-3.6.0/lib/jekyll/drops/excerpt_drop.rb
@@ -0,0 +1,16 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
+module Jekyll
+  module Drops
+    class ExcerptDrop < DocumentDrop
+      def layout
+        @obj.doc.data["layout"]
+      end
+
+      def excerpt
+        nil
+      end
+    end
+  end
+end
